04a67f2c066d6df6917754399942c21fa17be421
jdit9 <jhboska@gmail.com>
2020-03-25 15:15:27 -0400

    Update index.js
    add etsy site

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to my website.</p>
+    <h1>Hello</h1>
+    <p>Welcome to my website. There is not much here yet, but here is a link to my etsy store!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2><a href="https://www.etsy.com/shop/jordanshobbymart">Etsy Store</a></h2>
   </Layout>
 )
 
